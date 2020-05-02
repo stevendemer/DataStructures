0729aa25780f801dd1b6e5c5257c2f1ae8daa18d
stevendemer <devil.jaws@gmail.com>
2020-05-02 16:50:23 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #include "BST.h"
-#include "avl.h"
+// #include "avl.h"
 using namespace std;
 using namespace std::chrono;
 
@@ -19,20 +19,28 @@ string removePunc(string& str){
 }
 
 int main(int argc, char** argv){
-    AVL *avl = new AVL();
+    // AVL *avl = new AVL();
     BST *bst = new BST();
     auto start = high_resolution_clock::now();
     ifstream file("small-file.txt");
     string linestr;
-    int counter = 1000;
-    int choice;
-    while (getline(file, linestr) && counter--){
+    int counter = 5;
+    while (getline(file, linestr,' ') && counter--){
         string res = removePunc(linestr);
-        avl->insert(res);
+        bst->insert(res);
     }
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<seconds>(end - start);
     bst->inorder();
+    cout<<bst->search("well")<<endl;
+    cout<<bst->search("how")<<endl;
+    cout<<bst->search("steven")<<std::endl;
+    bst->deleteNode("EBook");
+    cout<<"After the deletion the tree is : "<<endl;
+    bst->inorder();
+    cout<<endl;
+    cout<<"------ ------- ------ \n";
+    bst->postorder();
     cout<<"It took "<<duration.count()<<" seconds to execute "<<endl;
 }
 
